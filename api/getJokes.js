25e52da56ecc3836edf336aedfecb85af3161d4f
David Fox <davidmfox123@gmail.com>
2024-09-23 09:00:19 +0000

    another test with removing drizzle

--- api/getJokes.js
+++ api/getJokes.js
@@ -1,19 +1,24 @@
 // import { drizzle } from 'drizzle-orm/vercel-postgres';
 import { Pool } from 'pg';
 import { jokes } from '../drizzle/schema';
-import { drizzle } from 'drizzle-orm';
+// import { drizzle } from 'drizzle-orm';
 
-const pool = new Pool({
-  connectionString: process.env.COCKROACH_DB_URL,
-});
+// const pool = new Pool({
+//   connectionString: process.env.COCKROACH_DB_URL,
+// });
 
-const db = drizzle(pool);
+// const db = drizzle(pool);
 
 export default async function handler(req, res) {
   if (req.method === 'GET') {
     try {
-      const result = await db.select().from(jokes).limit(10);
-      res.status(200).json(result);
+      // const result = await db.select().from(jokes).limit(10);
+      // res.status(200).json(result);
+
+      res.status(200).json({ 
+        message: `Hello from Vercel Serverless Function!`,
+        currentTime: formattedDate
+      });      
     } catch (error) {
       console.error('Error fetching jokes:', error);
       res.status(500).json({ error: 'Error fetching jokes' });
