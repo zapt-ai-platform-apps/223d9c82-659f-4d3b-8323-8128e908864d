01d9534957a2937d7e3a83137d01dec676c3d7f5
David Fox <davidmfox123@gmail.com>
2024-09-23 20:00:38 +0000

    add old schema

--- drizzle/schema.js
+++ drizzle/schema.js
@@ -1,9 +1,18 @@
-import { pgTable, bigint, text, timestamp, sql, uuid} from 'drizzle-orm/pg-core';
+// import { pgTable, text, timestamp} from 'drizzle-orm/pg-core';
+
+// export const jokes = pgTable('jokes', {
+//   id: bigint('id', { mode: 'number' }).primaryKey().notNull().default(sql`unique_rowid()`),
+//   setup: text('setup').notNull(),
+//   punchline: text('punchline').notNull(),
+//   createdAt: timestamp('created_at').defaultNow(),
+//   userId: uuid('user_id').notNull()
+// });
+
+import { pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
 
 export const jokes = pgTable('jokes', {
-  id: bigint('id', { mode: 'number' }).primaryKey().notNull().default(sql`unique_rowid()`),
+  id: serial('id').primaryKey(),
   setup: text('setup').notNull(),
   punchline: text('punchline').notNull(),
   createdAt: timestamp('created_at').defaultNow(),
-  userId: uuid('user_id').notNull()
 });
