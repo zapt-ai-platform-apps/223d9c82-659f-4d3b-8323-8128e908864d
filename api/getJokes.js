b11090338640fae63c6b527de8aa64319d714dbd
David Fox <davidmfox123@gmail.com>
2024-09-23 11:13:37 +0000

    another test

--- api/getJokes.js
+++ api/getJokes.js
@@ -3,21 +3,23 @@ import pg from 'pg';
 const { Client } = pg;
 import { jokes } from '../drizzle/schema.js';
 
-const client = new Client({
-  connectionString: process.env.COCKROACH_DB_URL,
-});
-
-const db = drizzle(client);
-
 export default async function handler(req, res) {
+  const client = new Client({
+    connectionString: process.env.COCKROACH_DB_URL,
+  });
 
   if (req.method === 'GET') {
     try {
+      await client.connect();
+      const db = drizzle(client);
+      
       const result = await db.select().from(jokes).limit(10);
-      res.status(200).json(result);   
+      res.status(200).json(result);
     } catch (error) {
       console.error('Error fetching jokes:', error);
       res.status(500).json({ error: 'Error fetching jokes' });
+    } finally {
+      await client.end();
     }
   } else {
     res.setHeader('Allow', ['GET']);
