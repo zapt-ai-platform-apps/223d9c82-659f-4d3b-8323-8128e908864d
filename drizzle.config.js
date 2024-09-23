7bcc281875e781e8a08af71d5b79d54de77be506
David Fox <davidmfox123@gmail.com>
2024-09-23 10:14:41 +0000

    try again with mjs

--- drizzle.config.js
+++ drizzle.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   dialect: "postgresql",
-  schema: "./drizzle/schema.js",
+  schema: "./drizzle/schema.mjs",
   out: "./drizzle",
   dbCredentials: {
     url: process.env.COCKROACH_DB_URL,
